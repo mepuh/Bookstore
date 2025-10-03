f74c811feb37387b59d4b3bae80d6b4c3f5c1ee6
mepuh <bhn266@myy.haaga-helia.fi>
2025-10-03 13:48:04 +0300

    Added Logger and replaced Autowired

--- src/main/java/hh/bookstore/BookstoreApplication.java
+++ src/main/java/hh/bookstore/BookstoreApplication.java
@@ -1,5 +1,7 @@
 package hh.bookstore;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +15,8 @@ import hh.bookstore.domain.CategoryRepository;
 @SpringBootApplication
 public class BookstoreApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(BookstoreApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
@@ -20,22 +24,43 @@ public class BookstoreApplication {
 	@Bean
 	public CommandLineRunner demoData(BookRepository repository, CategoryRepository categoryRepository) {
 		return args -> {
-			categoryRepository.save(new Category("Classic Literature"));
-			categoryRepository.save(new Category("Fantasy"));
-			categoryRepository.save(new Category("Mystery"));
-			categoryRepository.save(new Category("Science Fiction"));
-			categoryRepository.save(new Category("Horror"));
-			categoryRepository.save(new Category("Young Adult Fiction"));
-			categoryRepository.save(new Category("Children's Literature"));
-			categoryRepository.save(new Category("Non-Fiction"));
-			categoryRepository.save(new Category("Other"));
+			log.info("Save some categories");
+			Category classic = new Category("Classic Literature");
+			categoryRepository.save(classic);
+			Category fantasy = new Category("Fantasy");
+			categoryRepository.save(fantasy);
+			Category mystery = new Category("Mystery");
+			categoryRepository.save(mystery);
+			Category scienceFiction = new Category("Science Fiction");
+			categoryRepository.save(scienceFiction);
+			Category horror = new Category("Horror");
+			categoryRepository.save(horror);
+			Category youngAdult = new Category("Young Adult Fiction");
+			categoryRepository.save(youngAdult);
+			Category children = new Category("Children's Literature");
+			categoryRepository.save(children);
+			Category nonFiction = new Category("Non-Fiction");
+			categoryRepository.save(nonFiction);
+			Category other = new Category("Other");
+			categoryRepository.save(other);
 
-			repository.save(new Book("The Great Gatsby", "F. Scott Fitzgerald", 1925, "9780743273565", 10.99, categoryRepository.findByName("Classic Literature").get(0)));
-			repository.save(new Book("Harry Potter and the Philosopher's Stone", "J.K. Rowling", 1997, "9780747532699", 8.99, categoryRepository.findByName("Fantasy").get(0)));
-			repository.save(new Book("To Kill a Mockingbird", "Harper Lee", 1960, "9780061120084", 7.99, categoryRepository.findByName("Classic Literature").get(0)));
-			repository.save(new Book("The Fault in Our Stars", "John Green", 2012, "9780525478812", 9.99, categoryRepository.findByName("Young Adult Fiction").get(0)));
-			repository.save(new Book("Mauri Kunnaksen Koiramäki", "Mauri Kunnas", 1996, "9789511317134", 12.99, categoryRepository.findByName("Children's Literature").get(0)));
-			repository.save(new Book("Murder on the Orient Express", "Agatha Christie", 1934, "9780007119318", 6.99, categoryRepository.findByName("Mystery").get(0)));
+			log.info("Save some books");
+			repository.save(new Book("The Great Gatsby", "F. Scott Fitzgerald", 1925, "9780743273565", 10.99, classic));
+			repository.save(new Book("Harry Potter and the Philosopher's Stone", "J.K. Rowling", 1997, "9780747532699", 8.99, fantasy));
+			repository.save(new Book("To Kill a Mockingbird", "Harper Lee", 1960, "9780061120084", 7.99, classic));
+			repository.save(new Book("The Fault in Our Stars", "John Green", 2012, "9780525478812", 9.99, youngAdult));
+			repository.save(new Book("Mauri Kunnaksen Koiramäki", "Mauri Kunnas", 1996, "9789511317134", 12.99, children));
+			repository.save(new Book("Murder on the Orient Express", "Agatha Christie", 1934, "9780007119318", 6.99, mystery));
+
+			log.info("Fetch all categories");
+			for (Category category : categoryRepository.findAll()) {
+				log.info(category.toString());
+			}
+
+			log.info("Fetch all books");
+			for (Book book : repository.findAll()) {
+				log.info(book.toString());
+			}
 		};
 	}
 }
--- src/main/java/hh/bookstore/web/BookController.java
+++ src/main/java/hh/bookstore/web/BookController.java
@@ -1,6 +1,5 @@
 package hh.bookstore.web;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,14 +12,14 @@ import hh.bookstore.domain.CategoryRepository;
 
 @Controller
 public class BookController {
-    @Autowired
+   
     private BookRepository repository;
 
-    public BookController(BookRepository repository) {
+    public BookController(BookRepository repository, CategoryRepository categoryRepository) {
         this.repository = repository;
+        this.categoryRepository = categoryRepository;
     }
 
-    @Autowired
     private CategoryRepository categoryRepository;
 
     @GetMapping("/index")
